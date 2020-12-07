2709f722814893153246e4dd57f02cf15496af79
Viniciusalopes <suporte@viniciusalopes.com.br>
2020-12-07 03:59:04 -0300

    Included the DecimalFormat class

--- JFormattedMoney/src/br/com/vovolinux/JFormattedMoney.java
+++ JFormattedMoney/src/br/com/vovolinux/JFormattedMoney.java
@@ -3,13 +3,32 @@
  *  Licença    : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
  *  Criado em  : 06/12/2020 22:12:11 
  *  Projeto    : JFormattedMoney
- *  Versão     : 1.0.0
+ *  Versão     : 1.0.2
  *  ------------------------------------------------------------------------------------------------
  *  Propósito  : (PT-BR) Fornecer componente JFormattedTextField pronto para implementação, em 
  *                       formato de valor numérico e/ou monetário.
  *               (EN-US) Provide JFormattedTextField component ready for implementation, in numerical 
  *                       and/or monetary value format.
  *  ------------------------------------------------------------------------------------------------
+ *  Changelog:
+ *  Author     : Vovolinux
+ *  Date       : 2020-12-07
+ *  Version    : 1.0.2
+ *  Changes    : (PT-BR) Incluída a classe DecimalFormat.
+ *
+ *               (EN-BR) Included the DecimalFormat class.
+ *  -----------------------------------------------------------------------------------------------| 
+ *  Changelog:
+ *  Author     : Vovolinux
+ *  Date       : 2020-12-07
+ *  Version    : 1.0.1
+ *  Changes    : (PT-BR) Método KeyTyped alterado para não permitir a repetição de vírgula e de
+ *                       sinal de negativo;
+ *                       Criado o método toFloat() para retornar o valor do texto em float.
+ *
+ *               (EN-BR) KeyTyped method changed to not allow repetition of comma and negative sign;
+ *                       Created the toFloat () method to return the float text value.
+ *  -----------------------------------------------------------------------------------------------| 
  *  Changelog:
  *  Author     : Vovolinux
  *  Date       : YYYY-mm-dd
@@ -25,23 +44,26 @@ package br.com.vovolinux;
 
 import java.awt.Dimension;
 import java.awt.event.KeyEvent;
+import java.text.DecimalFormat;
 
 /**
  *
  * @author vovolinux
- * @version 1.0.0
- * @see <a href="https://github.com/Viniciusalopes/JFormattedMoney">github.com/Viniciusalopes/JFormattedMoney</a>
+ * @version 1.0.2
+ * @see
+ * <a href="https://github.com/Viniciusalopes/JFormattedMoney">github.com/Viniciusalopes/JFormattedMoney</a>
  */
 public class JFormattedMoney extends javax.swing.JFormattedTextField {
 
     /**
-     * Attributes
-     * Default values:
-     *   String validChars = "-01234.567,89"
-     *   String numberFormat = "#,##0.00"
+     * Attributes Default values: String validChars = "-01234.567,89" String numberFormat =
+     * "#,##0.00"
      */
     private String validChars = "-01234.567,89";
     private String numberFormat = "#,##0.00";
+    
+    // FONTE: https://javadevnotes.com/java-float-to-string-examples
+    private DecimalFormat decimalFormat = new DecimalFormat(numberFormat);
 
     /**
      * Constructor
@@ -61,12 +83,31 @@ public class JFormattedMoney extends javax.swing.JFormattedTextField {
     }
 
     /**
+     * Convert this text to float.
+     *
+     * @return
+     */
+    public float toFloat() {
+        return Float.parseFloat(this.getText().replace(".", "").replace(",", "."));
+    }
+
+    /**
      * Set up valid characters.
      *
      * @param validChars
      */
     public void setValidChars(String validChars) {
         this.validChars = validChars;
+    }
+
+    /**
+     * Set up text from float value.
+     *
+     * @param value
+     */
+    public void setText(float value) {
+
+        super.setText(decimalFormat.format(value));
     }
 
     /**
@@ -98,14 +139,14 @@ public class JFormattedMoney extends javax.swing.JFormattedTextField {
                 moneyKeyTyped(evt);
             }
         });
-        super.setText("0,00");
+        super.setText(decimalFormat.format(0));
     }
 
     /**
      * Event executed when gaining focus.
      */
     private void moneyFocusGained() {
-        if (super.getText().trim().equals("0,00")) {
+        if (this.toFloat() == 0) {
             super.setText(null);
         }
     }
@@ -117,6 +158,12 @@ public class JFormattedMoney extends javax.swing.JFormattedTextField {
      */
     private void moneyKeyTyped(KeyEvent evt) {
         char c = (char) evt.getKeyChar();
+        for (char t : this.getText().toCharArray()) {
+            if (t == ',' || t == '-') {
+                evt.setKeyChar((char) KeyEvent.VK_CLEAR); // don't repeat , and -
+                return;
+            }
+        }
         evt.setKeyChar(validChars.contains(c + "") ? c : (char) KeyEvent.VK_CLEAR);
     }
 
@@ -125,7 +172,7 @@ public class JFormattedMoney extends javax.swing.JFormattedTextField {
      */
     private void moneyFocusLost() {
         if (super.getText().trim().equals("")) {
-            super.setText("0,00");
+            super.setText(decimalFormat.format(0));
         }
     }
 }
